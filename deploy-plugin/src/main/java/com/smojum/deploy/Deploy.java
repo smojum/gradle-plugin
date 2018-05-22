408056af7b3e8e1df7cae4eaccc1801b638a06bf
smojum <santanu.mojumder@deancare.com>
2018-05-22 15:19:13 -0500

    initial checkin

---
+++ deploy-plugin/src/main/java/com/smojum/deploy/Deploy.java
@@ -0,0 +1,30 @@
+package com.smojum.deploy;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public class Deploy extends DefaultTask {
+	private String message;
+	private String recipient;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getRecipient() {
+		return recipient;
+	}
+
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+
+	@TaskAction
+	void sayGreeting() {
+		System.out.printf("%s, %s!\n", getMessage(), getRecipient());
+	}
+}
---
+++ deploy-plugin/src/main/java/com/smojum/deploy/DeployPlugin.java
@@ -0,0 +1,14 @@
+package com.smojum.deploy;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class DeployPlugin implements Plugin<Project> {
+	@Override
+	public void apply(Project project) {
+		project.getTasks().create("deploy", Deploy.class, (task) -> {
+			task.setMessage("Hello");
+			task.setRecipient("World");
+		});
+	}
+}
